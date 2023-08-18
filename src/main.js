2418cdae57f1f1797b55104b1ca29c07b0090af3
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-18 15:32:32 +0300

    Добавляет BoardView для общего контента

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import NewTaskButtonView from './view/new-task-button-view';
+import FilterView from './view/filter-view';
+import BoardView from './view/board-view';
 import {render} from './render.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 render(new NewTaskButtonView(), siteHeaderElement);
+render(new FilterView(), siteMainElement);
+render(new BoardView(), siteMainElement);
