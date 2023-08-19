1b1377bc402111da97f77351a02b1ebaadd913e0
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-19 18:09:33 +0300

    Добавляет моковые данные в TaskView

--- src/view/task-view.js
+++ src/view/task-view.js
@@ -1,7 +1,12 @@
 import {createElement} from '../render.js';
+import {humanizeTaskDueDate} from '../utils.js';
 
-function createTaskTemplate() {
-  return `<article class="card card--black">
+function createTaskTemplate(task) {
+  const {color, description, dueDate} = task;
+
+  const date = humanizeTaskDueDate(dueDate);
+
+  return `<article class="card card--${color}">
             <div class="card__form">
               <div class="card__inner">
                 <div class="card__control">
@@ -32,7 +37,7 @@ function createTaskTemplate() {
                 </div>
 
                 <div class="card__textarea-wrap">
-                  <p class="card__text">Example task with default color.</p>
+                  <p class="card__text">${description}</p>
                 </div>
 
                 <div class="card__settings">
@@ -40,7 +45,7 @@ function createTaskTemplate() {
                     <div class="card__dates">
                       <div class="card__date-deadline">
                         <p class="card__input-deadline-wrap">
-                          <span class="card__date">23 September</span>
+                          <span class="card__date">${date}</span>
                         </p>
                       </div>
                     </div>
@@ -53,8 +58,12 @@ function createTaskTemplate() {
 }
 
 export default class TaskView {
+  constructor({task}){
+    this.task = task;
+  }
+
   getTemplate() {
-    return createTaskTemplate();
+    return createTaskTemplate(this.task);
   }
 
   getElement() {
