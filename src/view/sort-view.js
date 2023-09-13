eda226522ff61a9b92f37ce0c8c901f2c1205001
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-09-13 11:16:21 +0300

    Добавляет data- атрибут, чтобы можно было определить выбранный тип сортировки

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,11 +1,12 @@
 import AbstractView from '../framework/view/abstract-view';
+import { SortType } from '../const';
 
 function createSortTemplate() {
   return (
     `<div class="board__sort-list">
-      <a href="#" class="board__sort-item">SORT BY DEFAULT</a>
-      <a href="#" class="board__sort-item">SORT BY DATE up</a>
-      <a href="#" class="board__sort-item">SORT BY DATE down</a>
+      <a href="#" class="board__sort-item" data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
+      <a href="#" class="board__sort-item" data-sort-type="${SortType.DATE_UP}">SORT BY DATE up</a>
+      <a href="#" class="board__sort-item" data-sort-type="${SortType.DATE_DOWN}">SORT BY DATE down</a>
     </div>`
   );
 }
